63bb44cb5f934ad83e3f541e284164abad9c9bde
Ihab Arnus <ihabarnus@gmail.com>
2024-07-21 12:25:18 +0300

    backend

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import expressAsyncHandler from "express-async-handler";
+import User from "../models/userModel";
+
+const protect = expressAsyncHandler(async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.userId).select("-password");
+  } catch (error) {
+    res.status(401);
+    throw new Error("Invalid Token");
+  }
+
+  next();
+});
+
+export { protect };
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+dotenv.config();
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server listening to route ${port}`));
